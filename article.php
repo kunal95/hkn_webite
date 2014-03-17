1b05c6527a2bfba05bece4303eb8191a6a0f8624
nonson <priyanshsingh@outlook.com>
2014-03-18 00:07:59 +0530

    php

---
+++ article.php
@@ -0,0 +1,69 @@
+
+<?php include ("header.php"); ?>
+
+
+      
+    <div class="container">
+        <div class = row>
+            <div class="col-md-9">
+                <div class="panel panel-default">
+                   <div class="panel-body"> 
+                
+                     <div class="page-header"> 
+                
+                       <img src="img/Untitled.png" class="featureImg" width="100%">
+                       </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                       
+                       <h4>HEADING</h4>
+                       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                    </div>
+                    
+                </div>
+                    
+            </div>
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                   <div class="panel-body"> 
+                
+                     <div class="page-header"> 
+                
+                       <img src="img/Untitled.png" class="featureImg" width="100%">
+                       </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                       
+                       <h4>HEADING</h4>
+                       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                    </div>
+                    
+                </div>
+                    
+            </div>
+        </div>
+        </div>
+
+      
+
+<div class="modal fade" id="contact" role = "dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4>
+                    Contact HKN    
+                </h4>
+            </div>   
+            <div class="modal-body">
+                <p>
+                SSSSSSSSSSSSSSSSSSSSSSMCNEKDLCXNLENKFNEcaedkbkasblcaskla
+                </p>
+            
+            </div>
+            <div class="modal-footer">
+                <a href ="#" class="btn btn-default"  data-dismiss="modal"> LAAALALLALALAL</a>
+                 <a href ="#" class="btn btn-primary"  data-dismiss="modal"> LAAALALLALALAL</a>
+            <p>ccccccccccccccc</p>
+            </div>
+        </div>
+    </div>
+</div> 
+    	
---
+++ header.php
@@ -0,0 +1,205 @@
+<?php echo'
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Lambda Eta - HKN</title>
+		<meta name="viewport" content="width  =  device-width, initial-scale = 1.0">
+		<link href="css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/unoSlider.min.css" type="text/css" media="screen" />
+        <link href="css/style.css" rel="stylesheet">	
+	</head>	
+    <body background = "img/bg.png"> 
+    	<ol class="breadcrumb">
+            <li><a href="#">HKN.org</a></li>
+			<li><a href="#">Bridge</a></li>
+            <li><a href="#">IEEE.org</a></li>
+        </ol>
+
+			<div class = "jumbotron">
+                <p> 
+                    <h1>This is the place where we add the title</h1> 
+
+                </p>
+			</div>
+	  <nav class="navbar navbar-default navbar-static-top " role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+  </div>
+ 
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">History</a></li>
+          <li><a href="#">Vision</a></li>
+          <li><a href="#">Mission</a></li>
+          <li><a href="#">Administration</a></li>
+        </ul>
+      </li>
+		<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Membership<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Benefits of Joining</a></li>
+          <li><a href="#">Procedure of joining</a></li>
+          <li><a href="#">Members Spotlight</a></li>
+         </ul>
+      </li>
+		<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Executive Committee <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Message from the branch Advisor</a></li>
+          <li><a href="#">Message from the President</a></li>
+          <li><a href="#">Lambda Eta Chapter Team</a></li>
+         </ul>
+		
+		<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Publications <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li class="dropdown-submenu"> <a tabindex="-1" href="#">E-Newsletter-COGNIZANCE</a>
+              <ul class="dropdown-menu">
+                  </li>
+                  <li class="dropdown-submenu"> <a href="#">More..</a>
+                      <ul class="dropdown-menu">
+                          <li><a href="#">January 2014</a>
+                          </li>
+                       </ul>
+                  </li>
+                  <li><a href="#">Subscribe</a>
+                  </li>
+                  <li><a href="#">Feedback/reviews</a>
+                  </li>
+              </ul>
+          </li>
+          <li><a href="#">Annual Chapter Report</a></li>
+		  <li><a href="#">Bridge</a></li>
+
+        </ul>
+      </li>
+		<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Awards and Honours<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Outstanding Chapter Award (2011-2012)</a></li>
+          <li><a href="#">Outstanding Student Award-Akshay Gupta</a></li>
+         </ul>
+		</li>	
+			
+		<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Participate<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li class="dropdown-submenu"> <a tabindex="-1" href="#">Contests</a>
+              <ul class="dropdown-menu">
+                  <li><a tabindex="-1" href="#">Cognizance Photo of the Month</a>
+                  </li>
+				  
+                  <li><a tabindex="-1" href="#">Cognizance Best Article of the Month</a>
+                  </li>
+				  
+              </ul>
+          </li>
+			<li class="dropdown-submenu"> <a tabindex="-1" href="#">HKN-Programs</a>
+
+			<ul class="dropdown-menu">
+                  <li><a tabindex="-1" href="#">HKN - Tutoring</a>
+                  </li>
+				  
+                  <li><a tabindex="-1" href="#">HKN - Workshops</a>
+                  </li>
+				  <li class="dropdown-submenu"> <a href="#">Student Mentorship Program</a>
+                      <ul class="dropdown-menu">
+                          <li><a href="#">Apply To Become a Memtor</a>
+                          </li>
+                          <li><a href="#">Apply To Become a Mentee</a>
+                          </li>
+                      </ul>
+                  </li>
+              </ul>
+			</li>
+        </ul>
+      </li>
+		
+		<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Activites<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li class="dropdown-submenu"> <a tabindex="-1" href="#">Past Events</a>
+              <ul class="dropdown-menu">
+				  
+                  <li><a tabindex="-1" href="#">Dr. Vikram Sarabhai Quiz</a>
+                  </li>
+				  
+				  <li><a tabindex="-1" href="#">All India Student Congress Workshop (2013)</a>
+                  </li>
+				  
+				  <li><a tabindex="-1" href="#">R10 Student/WIE/Gold Congress Workshop</a>
+                  </li>
+				  
+                  <li><a tabindex="-1" href="#">Spill the Beans</a>
+                  </li>
+				  
+                  <li><a tabindex="-1" href="#">Plantation Drive</a>
+                  </li>
+				  
+              </ul>
+          </li>
+			<li class="dropdown-submenu"> <a tabindex="-1" href="#">Upcoming Events</a></li>
+
+			</li>
+        </ul>
+      </li>
+		
+		<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Apply Online<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li class="dropdown-submenu"> <a tabindex="-1" href="#">Undergraduate Students</a>
+              <ul class="dropdown-menu">
+                  <li><a tabindex="-1" href="#">Application Procedure</a>
+                  </li>
+				  
+                  <li><a tabindex="-1" href="#">Apply Online</a>
+                  </li>
+				  
+              </ul>
+          </li>
+			<li class="dropdown-submenu"> <a tabindex="-1" href="#">Graduate Students</a>
+						<ul class="dropdown-menu">
+                  <li><a tabindex="-1" href="#">Application Procedure</a>
+                  </li>
+				  
+                  <li><a tabindex="-1" href="#">Apply Online</a>
+                  </li>
+				  
+              </ul>
+			</li>
+        </ul>
+      </li>
+		
+		<li class="dropdown">
+        <a href="#">Sponsor Us</a>
+        
+      </li>
+		
+		<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Contact Us<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li class="dropdown-submenu"> <a tabindex="-1" href="#">Feedback</a>
+             
+          </li>
+			<li class="dropdown-submenu"> <a tabindex="-1" href="#contact" data-toggle ="modal" >Contacts</a>
+
+			</li>
+        </ul>
+      </li>
+		
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+
+
+';?>
