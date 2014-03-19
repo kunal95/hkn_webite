3b09f74db6df8f248769d731adf5d4cc9e776534
kunal95 <kunsachdeva@gmail.com>
2014-03-20 00:21:35 +0530

    Create sca2012.php

---
+++ sca2012.php
@@ -0,0 +1,185 @@
+<html>
+<head>
+		<title>Lambda Eta - HKN</title>
+		<meta name="viewport" content="width  =  device-width, initial-scale = 1.0">
+		<link href="css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/unoSlider.min.css" type="text/css" media="screen" >
+        <link href="css/style.css" rel="stylesheet">	
+			<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+        <script src="js/unoSlider.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		// My Uno Slider
+		window.unoSlider = $('#sliderId').unoSlider();
+	});
+</script>
+	</head>	
+    
+<?php include ("header.php"); ?>
+<body>
+
+      
+    <div class="container">
+        <div class = row>
+            <div class="col-md-9">
+                <div class="panel panel-default">
+                   <div class="panel-body"> 
+                
+                     <div class="page-header"> 
+                <h2>Outstanding Chapter Plaque Award ceremony</h2>
+                     
+                       </div>
+                    <p>It is not only a matter of Pride but also a moment of celebration; The Lambda Eta chapter, 
+
+IEEE-HKN in Bharati Vidyapeeth’s College of Engineering has procured The Outstanding 
+
+Chapter Award in its very first year of initiation. The Outstanding Chapter Award is given 
+
+by IEEE-HKN to acknowledge excellence in colleges for its activities. To mark this event of 
+
+honour, an award ceremony was held on 15 may 2013, Wednesday in the conference hall, 
+
+BVCOE. Principal Sir, Prof. M. S. Bhattacharya graced the event with his humble presence. 
+
+Prof. Rahul Bahl, Branch counsellor, BVPIEEE along with various HODs and teachers also 
+
+dignified the ceremony with their distinguished presence. Further, all outgoing team members 
+
+of BVPIEEE and IEEE-HKN along with the newly elected team were also present. Himshikha 
+
+suhag hosted the ceremony.<br/>
+
+Prof. Rahul Bahl presented Principal Sir, Prof. M.S. Bhattacharya with a bouquet as a mark 
+
+of respect welcoming him for the ceremony. Later, Prof. M.S. Bhattacharya put forth The 
+
+Outstanding Chapter Award Plaque to Prof. Rahul Bahl, The Branch Counsellor, BVPIEEE 
+
+following which he addressed the audience and congratulated the team for the success and 
+
+enthusiasm they have shown till now. Rahul Bahl Sir also reiterated the fact that initiating IEEE-
+HKN in the college was not only the best foot forward but a golden foot forward. He hopes to 
+
+get The Outstanding Chapter Award for IEEE-HKN for many years to come.<br/><br/>
+
+To aggrandize the event, Mr Akshay Gupta, Former President, The Lambda Eta chapter IEEE-
+HKN, also attended the ceremony. He has been awarded with a gold medal for the best student 
+
+award. He has also been awarded The Outstanding Student award with special mention from 
+
+IEEE-HKN. Apart from this, he is currently pursuing his masters in computer science at Georgia 
+
+Tech. Moreover, he has published several papers in International Journals. Prof. Rahul Bahl 
+
+presented Akshay Gupta with a bouquet to honour him for his achievements. To commemorate 
+
+his success, Vishal Madan addressed him with few words of appreciation and a video clip was 
+
+played depicting his hard work and journey towards success. Akshay Sir became nostalgic 
+
+after watching the video. His eyes were filled with past memories and he thanked the IEEE-
+HKN team for the honour and respect. Moreover, he shared his experience being IEEE-HKN 
+
+President and wished best luck to the team to continue the good work. Lastly, Principal Sir 
+
+awarded certificates to the outgoing IEEE-HKN team for the excellent work.</p>
+                     </div>
+                    
+                </div>
+                    
+            </div>
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                   <div class="panel-body"> 
+                
+                     <div class="page-header"> 
+                       </div>
+                   It is not only a matter of Pride but also a moment of celebration; The Lambda Eta chapter, 
+
+IEEE-HKN in Bharati Vidyapeeth’s College of Engineering has procured The Outstanding 
+
+Chapter Award in its very first year of initiation. The Outstanding Chapter Award is given 
+
+by IEEE-HKN to acknowledge excellence in colleges for its activities. To mark this event of 
+
+honour, an award ceremony was held on 15 may 2013, Wednesday in the conference hall, 
+
+BVCOE. Principal Sir, Prof. M. S. Bhattacharya graced the event with his humble presence. 
+
+Prof. Rahul Bahl, Branch counsellor, BVPIEEE along with various HODs and teachers also 
+
+dignified the ceremony with their distinguished presence. Further, all outgoing team members 
+
+of BVPIEEE and IEEE-HKN along with the newly elected team were also present. Himshikha 
+
+suhag hosted the ceremony.<br/>
+
+Prof. Rahul Bahl presented Principal Sir, Prof. M.S. Bhattacharya with a bouquet as a mark 
+
+of respect welcoming him for the ceremony. Later, Prof. M.S. Bhattacharya put forth The 
+
+Outstanding Chapter Award Plaque to Prof. Rahul Bahl, The Branch Counsellor, BVPIEEE 
+
+following which he addressed the audience and congratulated the team for the success and 
+
+enthusiasm they have shown till now. Rahul Bahl Sir also reiterated the fact that initiating IEEE-
+HKN in the college was not only the best foot forward but a golden foot forward. He hopes to 
+
+get The Outstanding Chapter Award for IEEE-HKN for many years to come.<br/><br/>
+
+To aggrandize the event, Mr Akshay Gupta, Former President, The Lambda Eta chapter IEEE-
+HKN, also attended the ceremony. He has been awarded with a gold medal for the best student 
+
+award. He has also been awarded The Outstanding Student award with special mention from 
+
+IEEE-HKN. Apart from this, he is currently pursuing his masters in computer science at Georgia 
+
+Tech. Moreover, he has published several papers in International Journals. Prof. Rahul Bahl 
+
+presented Akshay Gupta with a bouquet to honour him for his achievements. To commemorate 
+
+his success, Vishal Madan addressed him with few words of appreciation and a video clip was 
+
+played depicting his hard work and journey towards success. Akshay Sir became nostalgic 
+
+after watching the video. His eyes were filled with past memories and he thanked the IEEE-
+HKN team for the honour and respect. Moreover, he shared his experience being IEEE-HKN 
+
+President and wished best luck to the team to continue the good work. Lastly, Principal Sir 
+
+awarded certificates to the outgoing IEEE-HKN team for the excellent work.   </div>
+                    
+                </div>
+                    
+            </div>
+        </div>
+        </div>
+
+      
+
+<div class="modal fade" id="contact" role = "dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4>
+                    Contact HKN    
+                </h4>
+            </div>   
+            <div class="modal-body">
+                <p>
+                SSSSSSSSSSSSSSSSSSSSSSMCNEKDLCXNLENKFNEcaedkbkasblcaskla
+                </p>
+            
+            </div>
+            <div class="modal-footer">
+                <a href ="#" class="btn btn-default"  data-dismiss="modal"> LAAALALLALALAL</a>
+                 <a href ="#" class="btn btn-primary"  data-dismiss="modal"> LAAALALLALALAL</a>
+            <p>ccccccccccccccc</p>
+            </div>
+        </div>
+    </div>
+</div> 
+    	
+</body>
+</html>
