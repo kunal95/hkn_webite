35134cae7568aff0efd9cd278d53a19f80668bf8
kunal95 <kunsachdeva@gmail.com>
2014-03-21 00:28:45 +0530

    Create HKN_WO3.php

---
+++ HKN_WO3.php
@@ -0,0 +1,98 @@
+<head>
+		<title>Lambda Eta - HKN</title>
+		<meta name="viewport" content="width  =  device-width, initial-scale = 1.0">
+		<link href="css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/unoSlider.min.css" type="text/css" media="screen" >
+        <link href="css/style.css" rel="stylesheet">	
+			<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+        <script src="js/unoSlider.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		// My Uno Slider
+		window.unoSlider = $('#sliderId').unoSlider();
+	});
+</script>
+	</head>	
+	<?php include ("header.php"); ?>
+	
+	
+	      
+	    <div class="container">
+	        <div class = row>
+	            <div class="col-md-9">
+	                <div class="panel panel-default">
+	                   <div class="panel-body"> 
+	                
+	                     <div class="page-header"> 
+	                
+					    <h1>IEEE HKN ORGANISATION</h1>
+	                       </div>
+	
+			    <p>IEEE-Eta Kappa Nu consists of about 200 university chapters, several thousands of members, a variety of committees, many active member volunteers, and thousands of student members. </p>
+
+<p>The overall governance of IEEE-HKN is the responsibility of the Board of Governors, a volunteer organization of IEEE-HKN members that have prominent positions in academia and industry. The Board consists of a president, vice president, past president, secretary, and treasurer, each of whom serves a one-year term. Seven directors, four representing each of IEEE-HKN's geographic regions and three at-large members, serve one three-year term. </p>
+
+<p>IEEE-HKN Headquarters is managed by the IEEE Educational Activities Board. Nancy M. Ostin serves as the Director of IEEE-HKN. The Director is the chief operating executive of IEEE-HKN and is supported by IEEE staff members who perform the myriad of functions necessary for the success of IEEE-HKN. </p>
+
+<p>IEEE-HKN has approximately 200 chartered chapters. Each chapter is student-led and capably guided by a faculty advisor. Students having achieved the required academic standing and also having significant leadership and exceptional character qualities are invited to become IEEE-HKN members. </p>
+
+<p>New chapters are established upon letter request from the institution's Department Chair, Dean of Engeneering, School Cancellor, and other university administrators, and which meet the requirements of a recognized regional or professional accrediting agency in the IEEE-designated fields of interest. The IEEE-designated fields of interest include: Engineering, Computer Sciences and Information Technology, Physical Sciences, Biological and Medical Sciences, Mathematics, Technical Communications, Education,Management, Law and Policy. The chapters are supported by IEEE-HKN Headquarters and the Board of Governors' regional directors. </p>
+
+<p>The IEEE-HKN college chapters perform a variety of service functions. Often, they help their department, college, or community. IEEE-HKN has eight standing committees consisting of dedicated volunteers. The chair of each committee is appointed by the president of the Board of Governors. Committee members are normally members of IEEE-HKN with a particular interest or talent related to the committee's purpose. IEEE-HKN encourages members to become involved by volunteering to join an IEEE-HKN committee</p>
+			    <h5>Present IEEE-HKN committees consist of the following:</h5>
+	                    <ul style="list-stye-type:disc">
+				<li><a href="#">Alton B. Zerby and Carl T. Koerner Outstanding Electrical and Computer Engineering Student Award</a></li>
+				<li><a href="#">Distinguished Service Award</a></li>
+				<li><a href="#">Eminent Member Award</a></li>
+				<li><a href="#">Outstanding Chapter Award</a></li>
+				<li><a href="#">Outstanding Young Electrical and Computer Engineer Award</a></li>
+				<li><a href="#">C. Holmes MacDonald Outstanding Teaching Award</a></li>
+				<li><a href="#">Vladimir Karapetoff Outstanding Technical Achievement Award</a></li>
+				<li><a href="#">Induction Ritual</a></li>
+			    </ul>
+			    </div>
+	                </div>
+	                    
+	            </div>
+	            <div class="col-md-3">
+	                <div class="panel panel-default">
+	                   <div class="panel-body"> 
+	                
+	                     <div class="page-header"> 
+	                
+	                				    <h4>IEEE HKN ORGANISATION</h4>
+	                       </div>
+<p>The overall governance of IEEE-HKN is the responsibility of the Board of Governors, a volunteer organization of IEEE-HKN members that have prominent positions in academia and industry. The Board consists of a president, vice president, past president, secretary, and treasurer, each of whom serves a one-year term. Seven directors, four representing each of IEEE-HKN's geographic regions and three at-large members, serve one three-year term. </p>
+	                    </div>
+	                    
+	                </div>
+	                    
+	            </div>
+	        </div>
+	        </div>
+	
+	      
+	
+	<div class="modal fade" id="contact" role = "dialog">
+	    <div class="modal-dialog">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h4>
+	                    Contact HKN    
+	                </h4>
+	            </div>   
+	            <div class="modal-body">
+	                <p>
+	                SSSSSSSSSSSSSSSSSSSSSSMCNEKDLCXNLENKFNEcaedkbkasblcaskla
+	                </p>
+	            
+	            </div>
+	            <div class="modal-footer">
+	                <a href ="#" class="btn btn-default"  data-dismiss="modal"> LAAALALLALALAL</a>
+	                 <a href ="#" class="btn btn-primary"  data-dismiss="modal"> LAAALALLALALAL</a>
+	            <p>ccccccccccccccc</p>
+	            </div>
+	        </div>
+	    </div>
+	</div> 
