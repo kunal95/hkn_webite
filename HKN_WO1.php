0c1d2574824911f571892d4af0392f6e788163ae
kunal95 <kunsachdeva@gmail.com>
2014-03-21 00:25:29 +0530

    Create HKN_WO1.php

---
+++ HKN_WO1.php
@@ -0,0 +1,85 @@
+<head>
+		<title>Lambda Eta - HKN</title>
+		<meta name="viewport" content="width  =  device-width, initial-scale = 1.0">
+		<link href="css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/unoSlider.min.css" type="text/css" media="screen" >
+        <link href="css/style.css" rel="stylesheet">	
+			<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+        <script src="js/unoSlider.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		// My Uno Slider
+		window.unoSlider = $('#sliderId').unoSlider();
+	});
+</script>
+	</head>	
+	<?php include ("header.php"); ?>
+	
+	
+	      
+	    <div class="container">
+	        <div class = row>
+	            <div class="col-md-9">
+	                <div class="panel panel-default">
+	                   <div class="panel-body"> 
+	                
+	                     <div class="page-header"> 
+	                
+	               	    <h1>IEEE-HKN MISSION AND PURPOSE</h1>
+	                       </div>
+		
+	                    <p>IEEE-Eta Kappa Nu is the student honor society of IEEE and is dedicated to encouraging and recognizing excellence in the IEEE-designated fields of interest. Members consist of students, alumni, and other professionals who have demonstrated exceptional academic and professional accomplishments. Student members are selected on the basis of scholastic standing, character, and leadership. Through a variety of service programs and leadership training, student members develop lifelong skills that earmark them for prominent positions in industry and academia. Members are much better prepared for the post-college world and are especially valuable and attractive to employers. They are recognized as extremely capable, both technically and professionally. 
+				</p>
+
+<p>After graduation, members continue their involvement through their university chapters, volunteering for IEEE-HKN committees and receiving information on IEEE-HKN activities. </p>
+
+<p>To further recognize excellence, IEEE-HKN conducts award programs to recognize professional leaders, exceptional educators, outstanding students, young engineering professionals, outstanding chapters, and effective faculty advisors. </p>
+
+<p>For more than 100 years, Eta Kappa Nu has embraced excellence. In the years ahead, IEEE-HKN will continue to reinvent itself to meet the needs of its members, the institutions they serve, and our overall society. </p>
+			     
+			    </div>
+	                    
+	                </div>
+	                    
+	            </div>
+	            <div class="col-md-3">
+	                <div class="panel panel-default">
+	                   <div class="panel-body"> 
+	                
+	                     <div class="page-header"> 
+	               
+	                       </div>
+IEEE-Eta Kappa Nu is the student honor society of IEEE and is dedicated to encouraging and recognizing excellence in the IEEE-designated fields of interest. Members consist of students, alumni, and other professionals who have demonstrated exceptional academic and professional accomplishments. Student members are selected on the basis of scholastic standing, character, and leadership. Through a variety of service programs and leadership training, student members develop lifelong skills that earmark them for prominent positions in industry and academia. Members are much better prepared for the post-college world and are especially valuable and attractive to employers. They are recognized as extremely capable, both technically and professionally. 
+				                    </div>
+	                    
+	                </div>
+	                    
+	            </div>
+	        </div>
+	        </div>
+	
+	      
+	
+	<div class="modal fade" id="contact" role = "dialog">
+	    <div class="modal-dialog">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h4>
+	                    Contact HKN    
+	                </h4>
+	            </div>   
+	            <div class="modal-body">
+	                <p>
+	                SSSSSSSSSSSSSSSSSSSSSSMCNEKDLCXNLENKFNEcaedkbkasblcaskla
+	                </p>
+	            
+	            </div>
+	            <div class="modal-footer">
+	                <a href ="#" class="btn btn-default"  data-dismiss="modal"> LAAALALLALALAL</a>
+	                 <a href ="#" class="btn btn-primary"  data-dismiss="modal"> LAAALALLALALAL</a>
+	            <p>ccccccccccccccc</p>
+	            </div>
+	        </div>
+	    </div>
+	</div> 
