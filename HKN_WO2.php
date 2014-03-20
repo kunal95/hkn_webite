cebf4a32ff543cfd49855a6adc8ba4d521b415b1
kunal95 <kunsachdeva@gmail.com>
2014-03-21 00:27:27 +0530

    Create HKN_WO2.php

---
+++ HKN_WO2.php
@@ -0,0 +1,105 @@
+<head>
+		<title>Lambda Eta - HKN</title>
+		<meta name="viewport" content="width  =  device-width, initial-scale = 1.0">
+		<link href="css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/unoSlider.min.css" type="text/css" media="screen" >
+        <link href="css/style.css" rel="stylesheet">	
+			<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+        <script src="js/unoSlider.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		// My Uno Slider
+		window.unoSlider = $('#sliderId').unoSlider();
+	});
+</script>
+	</head>	
+	<?php include ("header.php"); ?>
+	
+	
+	      
+	    <div class="container">
+	        <div class = row>
+	            <div class="col-md-9">
+	                <div class="panel panel-default">
+	                   <div class="panel-body"> 
+	                
+	                     <div class="page-header"> 
+	                
+	                       <h1>HKN AND IEEE-HKN HISTORY</h1>
+	                       </div>
+			    
+	                       <p>Eta Kappa Nu (HKN) is the result of the vision of Maurice L. Carr, an electrical engineering student at the University of Illinois.He shared that vision with nine other students who together founded the organization on 28 October 1904. The original perspective regarding qualifications for membership was that scholarship was important but that the selection of students with the character and attitude that would make them probable leaders in the profession was even more important. </p>
+
+<p>This whole matter of required qualifications for membership was debated in the early years, with the conclusion that HKN would be an Electrical Engineering Honor Society and that scholarship should be the primary requirement, with character and other personal attributes secondary. </p>
+
+<p>In the late 1930s, the national officers recommended that all colleges set the scholastic qualifications for membership to be the upper one-fourth of the junior EE class and the upper one-third of the senior EE class. In 1947, that qualification was made mandatory for all chapters by its incorporation in the national Constitution. </p>
+
+<p>Maurice Carr later acknowledged that one of the reasons for the founding of Eta Kappa Nu was to garner jobs for its members, but he also saw the organization as an incentive and inspiration for the individual — not just members, but all engineering students. He recognized that the prestige and reputation of the society rested on the character of its members after graduation. Significantly, in 1939, Carr wrote that "Eta Kappa Nu grew because there has always been many members who have been willing and eager to serve it loyally and unselfishly." This is as true today as it was in 1939. </p>
+
+<p>The Wheatstone Bridge symbol was selected as the emblem of HKN by the founding group, even though Maurice Carr preferred the Caduceus. In later years, Carr acknowledged that he had not realized that the medical profession had already selected his preferred symbol. The HKN Coat of Arms or Shield, as it is often called, (discussed in the Induction Ritual) was initiated in 1927. </p>
+
+<p>The Induction Ceremony was secret from the start of HKN until the 1959 Convention, where the chapters voted to make secrecy optional for each chapter. This gave chapters the opportunity to conduct induction ceremonies with parents and friends in attendance. Secrecy is no longer an option, and Eta Kappa Nu is not a secret society in any respect. </p>
+
+<p>Similarly, in the early years of its existence, HKN members were almost exclusively male, as there were no female EE students. It was natural that the governing documents, the Induction Ceremony, and other materials would make reference to members as being of male gender. However, HKN has no membership limitations with regards to gender, race, religion, or nationality. </p>
+
+<p>Eta Kappa Nu's first publication was a small four-page leaflet titled "The Electrical Field," issued in the spring of 1906. It was devoted almost entirely to the subject of employment. It contained commentary about four companies that employed electrical engineers, and it included the list of names of the members graduating from both the University of Illinois and the Purdue University chapters. This leaflet was published annually until 1909, when it began to be published semi-annually. In 1913, "The Electrical Field" was renamed "The Bridge" and was published annually. Today, "The Bridge" is published twice a year. </p>
+
+<p>As an industry and a profession, electrical engineering grew relatively slowly as the electrical power and telecommunications infrastructure was accepted and developed in the early decades of the 20th century. Substantial change occurred during World War II and proved to be a substantial growth period for electrical engineering. </p>
+
+<p>Huge demand existed for power systems to support the massive manufacturing infrastructure created by the War. Overwhelming innovation occurred in electrical, electronic, and communications equipment and systems for war-time applications on ground, sea, and in the air. After World War II, the GI Bill offered college education opportunities to millions of returning young service men and women, with many entering the field of electrical engineering. </p>
+
+<p>The world suddenly realized and appreciated the wonderful benefits of electricity and electronics. Since the War, the electronics/electrical industry has become a driving economic force, and Eta Kappa Nu has fostered many of the leaders that have established this industry. </p>
+
+<p>In the last decade, both academia and HKN have acknowledged the important role of the computer. Many academic departments have changed their name from Electrical Engineering to Electrical and Computer Engineering. HKN has also changed its focus to include computer engineering. Both academia and industry value HKN and the many accomplishments of its 200,000 members. </p>
+
+<p>In 2006, discussions between the Eta Kappa Nu Association (HKN), the Institute of Electrical and Electronics Engineers, Inc. (IEEE), and the IEEE Foundation (IEEE Foundation) began. In 2007, a Memorandum of Understanding (MOU) was signed which was a non-binding expression of the Parties' intent to merge HKN with and into IEEE, and for the IEEE Foundation to administer the funds to support continued HKN activities within IEEE following the merger. </p>
+
+<p>On 14 February 2009, the merger agreement was made and entered into by and among the three Parties. On 1 September 2010, the merger became effective and what was once known as Eta Kappa Nu (HKN) is now known as IEEE-Eta Kappa Nu (IEEE-HKN). </p>
+			    </div>
+	                    
+	                </div>
+	                    
+	            </div>
+	            <div class="col-md-3">
+	                <div class="panel panel-default">
+	                   <div class="panel-body"> 
+	                
+	                     <div class="page-header"> 
+	                
+<h4>HKN AND IEEE-HKN HISTORY</h4>
+	                       </div>
+	                       <p>Eta Kappa Nu (HKN) is the result of the vision of Maurice L. Carr, an electrical engineering student at the University of Illinois.He shared that vision with nine other students who together founded the organization on 28 October 1904. The original perspective regarding qualifications for membership was that scholarship was important but that the selection of students with the character and attitude that would make them probable leaders in the profession was even more important. </p>
+
+	                    </div>
+	                    
+	                </div>
+	                    
+	            </div>
+	        </div>
+	        </div>
+	
+	      
+	
+	<div class="modal fade" id="contact" role = "dialog">
+	    <div class="modal-dialog">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h4>
+	                    Contact HKN    
+	                </h4>
+	            </div>   
+	            <div class="modal-body">
+	                <p>
+	                SSSSSSSSSSSSSSSSSSSSSSMCNEKDLCXNLENKFNEcaedkbkasblcaskla
+	                </p>
+	            
+	            </div>
+	            <div class="modal-footer">
+	                <a href ="#" class="btn btn-default"  data-dismiss="modal"> LAAALALLALALAL</a>
+	                 <a href ="#" class="btn btn-primary"  data-dismiss="modal"> LAAALALLALALAL</a>
+	            <p>ccccccccccccccc</p>
+	            </div>
+	        </div>
+	    </div>
+	</div> 
