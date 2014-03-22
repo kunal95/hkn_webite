6711e1994fe8365c5d03e5e8c5c85425d15bd151
KushagraB <emailkushagra@gmail.com>
2014-03-22 22:34:35 +0530

    Chap_Award

---
+++ Chap_Award.php
@@ -0,0 +1,90 @@
+<head>
+		<title>Lambda Eta - HKN</title>
+		<meta name="viewport" content="width  =  device-width, initial-scale = 1.0">
+		<link href="css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/unoSlider.min.css" type="text/css" media="screen" >
+        <link href="css/style.css" rel="stylesheet">	
+			<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+        <script src="js/unoSlider.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		// My Uno Slider
+		window.unoSlider = $('#sliderId').unoSlider();
+	});
+</script>
+	</head>		
+	<?php include ("header.php"); ?>
+	
+	
+	      
+	    <div class="container">
+	        <div class = row>
+	            <div class="col-md-9">
+	                <div class="panel panel-default">
+	                   <div class="panel-body"> 
+	                
+	                     <div class="page-header"> 
+	                
+	                       <img src="img/Untitled.png" class="featureImg" width="100%">
+	                       </div>
+			    <h4>IEEE-HKN bags the Outstanding Chapter Award yet again !!</h4>
+	                    <p>Adding yet another jewel to its crown of glory IEEE-HKN BVCOE was given the outstanding chapter award. This honour was bestowed upon the lambda-eta chapter for the second time, since the its very establishment. The EXECOMM was enthralled by the news and people rejoiced.</p>
+
+<p>The purpose of the Outstanding Chapter Award is to recognize excellence in college chapters for their activities. The award is based on the contents and description of chapter activities that are contained in the Annual Chapter Report. The Annual Chapter Reports are due by June 30 of each year and summarize all activities for the previous academic year from July 1 to June 30.</p>
+
+<p>In bestowing an award, the standing committee attaches less importance to the number of a chapter's activities than to their nature and quality. Of critical concern to the committee in judging a chapter are activities to improve professional development, to raise instructional and institutional standards, to encourage scholarship and creativity, to provide a public service, and generally to further the established goals of IEEE-HKN. </p>
+
+<p>Though college chapters are under no obligation to contend for this award, all chapters are required to submit at least the short-form Annual Chapter Report summarizing their officers and activities. Chapters wishing to contend for the award are encouraged to submit a more comprehensive report covering their activities. </p>
+
+<p>It was a moment of ecstasy for the people of the college and the people dedicated to working for the chapter. We hope that IEEE-HKN continues to prove its mantel in the years to follow and every new EXECOMM has something more to offer to the development of this chapter. </p>
+        	                    </div>
+	                    
+	                </div>
+	                    
+	            </div>
+	            <div class="col-md-3">
+	                <div class="panel panel-default">
+	                   <div class="panel-body"> 
+	                
+	                     <div class="page-header"> 
+	                
+	                       <img src="img/Untitled.png" class="featureImg" width="100%">
+	                       </div>
+	                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+	                       
+	                       <h4>HEADING</h4>
+	                       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+	                    </div>
+	                    
+	                </div>
+	                    
+	            </div>
+	        </div>
+	        </div>
+	
+	      
+	
+	<div class="modal fade" id="contact" role = "dialog">
+	    <div class="modal-dialog">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h4>
+	                    Contact HKN    
+	                </h4>
+	            </div>   
+	            <div class="modal-body">
+	                <p>
+	                		SSSSSSSSSSSSSSSSSSSSSSMCNEKDLCXNLENKFNEcaedkbkasblcaskla
+	                </p>
+	            
+	            </div>
+	            <div class="modal-footer">
+	                <a href ="#" class="btn btn-default"  data-		dismiss="modal"> LAAALALLALALAL</a>
+	                 <a href ="#" class="btn btn-primary"  data-		dismiss="modal"> LAAALALLALALAL</a>
+	            <p>ccccccccccccccc</p>
+	            </div>
+	        </div>
+	    </div>
+	</div> 
+
