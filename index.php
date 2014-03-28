8aff9fe7e1bfc049aacd04bee79e8d2e900da223
kunal95 <kunsachdeva@gmail.com>
2014-03-29 03:06:06 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,131 @@
+<html>
+<head>
+		<title>Lambda Eta - HKN</title>
+		<meta name="viewport" content="width  =  device-width, initial-scale = 1.0">
+		<link href="css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/unoSlider.min.css" type="text/css" media="screen" >
+        <link href="css/style.css" rel="stylesheet">	
+			<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+
+
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.totemticker.js"></script>
+	<link rel="stylesheet" href="css/unoSlider.min.css" type="text/css" media="screen" />
+	<script type="text/javascript">
+		$(function(){
+			$('#vertical-ticker').totemticker({
+				row_height	:	'100px',
+				next		:	'#ticker-next',
+				previous	:	'#ticker-previous',
+				stop		:	'#stop',
+				start		:	'#start',
+				mousestop	:	true,
+			});
+	
+		// My Uno Slider
+		window.unoSlider = $('#sliderId').unoSlider();
+	});
+	</script>
+	        <script src="js/unoSlider.js"></script>
+	</head>	
+    
+<?php include ("header.php"); ?>
+<body>
+<style>
+#headers{width:100%;height:300px;margin-bottom: 30px;margin-top: 30px;}
+.box{width:32.9%;height:100%;float:left;border:2px solid black;margin-left: 3px;border-radius: 10px;padding:7px;}
+</style>
+      
+    <div class="container">
+        <div class = row>
+            <div class="col-md-9">
+                <div class="panel panel-default">
+                   <div class="panel-body"> 
+                
+                     <div class="page-header"> 
+                
+               <div id="sliderId">
+                        <ul>
+                            <li class="current">HERE GOES THE CONTENT FOR THE SLIDER #1</li>
+                            <li>HERE GOES THE CONTENT FOR THE SLIDER #2</li>
+                            <li>HERE GOES THE CONTENT FOR THE SLIDER #3</li>
+                        </ul>
+               </div>
+			   <div id="headers">
+			   <div class="box">
+			   <img src="abc.jpg" width="250" height="150"/>
+			   <br/>
+			   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s <a href="#">Read More >></a>
+			   </div>
+				<div class="box">
+			   <img src="abc.jpg" width="250" height="150"/>
+			   <br/>
+			   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s <a href="#">Read More >></a>
+			   </div>
+				<div class="box">
+			   <img src="abc.jpg" width="250" height="150"/>
+			   <br/>
+			   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s <a href="#">Read More >></a>
+			   </div>			   
+                       </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                       
+                       <h4>HEADING</h4>
+                       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                    </div>
+                    
+                </div>
+                    
+            </div>
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                   <div class="panel-body"> 
+                
+                     
+		<ul id="vertical-ticker">
+			<li>One Time</li>
+			<li>Baby</li>
+			<li>One Less Lonely Girl</li>
+			<li>Somebody to Love</li>
+			<li>Eenie Meenie</li>
+			<li>Never Say Never</li>
+			<li>U Smile</li>
+		</ul>
+	
+	</div>
+                    
+                </div>
+                    
+            </div>
+        </div>
+        </div>
+
+      
+
+<div class="modal fade" id="contact" role = "dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4>
+                    Contact HKN    
+                </h4>
+            </div>   
+            <div class="modal-body">
+                <p>
+                SSSSSSSSSSSSSSSSSSSSSSMCNEKDLCXNLENKFNEcaedkbkasblcaskla
+                </p>
+            
+            </div>
+            <div class="modal-footer">
+                <a href ="#" class="btn btn-default"  data-dismiss="modal"> LAAALALLALALAL</a>
+                 <a href ="#" class="btn btn-primary"  data-dismiss="modal"> LAAALALLALALAL</a>
+            <p>ccccccccccccccc</p>
+            </div>
+        </div>
+    </div>
+</div> 
+    	
+</body>
+</html>
